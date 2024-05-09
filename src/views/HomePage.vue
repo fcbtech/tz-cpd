5df8b4c4c2aa638c3f743cef468884e85c850291
Ayush Dubey <ayush7ad6@Ayushs-MacBook-Air.local>
2024-05-09 11:20:59 +0530

    Changes in homepage

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -1,15 +1,38 @@
 <template>
-    <div>
-        <NavBar />
-        <v-container style="margin-top: 70px;">
-            {{ useProfileStore().getUserFirstName() + ' ' + useProfileStore().getUserLastName() }}
-            <v-row>
-            </v-row>
-        </v-container>
-    </div>
+  <div>
+    <NavBar />
+    <v-container style="margin-top: 70px;">
+      {{ useProfileStore().getUserFirstName() + ' ' + useProfileStore().getUserLastName() }}
+      <v-row>
+        <v-col>
+          <span>
+            Connect your Tally
+          </span>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-text-field v-model="tallyIP" label="Tally IP" variant="outlined" :height="70" value="localhost"
+            required></v-text-field>
+        </v-col>
+        <v-col>
+          <v-text-field v-model="tallyPort" type="number" value="9000" label="Port Number" variant="outlined"
+            required></v-text-field>
+        </v-col>
+        <v-col>
+          <v-checkbox v-model="isTally3dot0" label="Tally 3.0" value="Tally 3.0"></v-checkbox>
+        </v-col>
+      </v-row>
+    </v-container>
+  </div>
 </template>
 
 <script setup>
+import { ref } from 'vue'
 import NavBar from '@/components/NavBar.vue';
 import { useProfileStore } from '@/piniaStore/common/auth/profile';
+
+const isTally3dot0 = ref(false)
+const tallyIP = ref('localhost')
+const tallyPort = ref(9000)
 </script>
