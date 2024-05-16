52380277b50ae685a7cc21737b3bc42dfda0a5f4
Ayush Dubey <ayush7ad6@Ayushs-MacBook-Air.local>
2024-05-16 09:53:30 +0530

    Changes to have actual columns

--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -4,29 +4,31 @@
       <NavBar />
     </div>
     <div style="margin-top:100px;">
-      <v-text-field
+      <!-- <v-text-field
         v-model="search"
         label="Search"
         prepend-inner-icon="mdi-magnify"
         variant="outlined"
         hide-details
         single-line
-      ></v-text-field>
-      <v-data-table item-key="name" :search="search" :headers="headers" :items="filteredData" :sort-by="[{ key: 'calories', order: 'asc' }]">
+      ></v-text-field> -->
+      <v-data-table hover fixed-header height="500" density="compact" item-key="name" :search="search" :headers="headers" :items="filteredData" :sort-by="[{ key: 'calories', order: 'asc' }]">
         <template
-            v-for="(header, i) in headers.slice(0, 5)"
-            v-slot:[`header.${header.key}`]="{ }"
-          >
-            {{ header.title }}
-            <div @click.stop :key="i">
-              <v-text-field
-                :key="i"
-                v-model="multiSearch[header.key]"
-                class="text-capitalize pa"
-                type="text"
-                :placeholder="header.key.charAt(0).toUpperCase() + header.key.slice(1)"
-              ></v-text-field>
-            </div>
+          v-for="(header, i) in headers.slice(0, headers.length - 1)"
+          v-slot:[`header.${header.key}`]="{ }"
+        >
+        <span class="text-h5">
+          {{ header.title }}
+        </span>
+          <div @click.stop :key="i">
+            <v-text-field
+              :key="i"
+              v-model="multiSearch[header.key]"
+              class="text-capitalize pa"
+              type="text"
+              :placeholder="header.key.charAt(0).toUpperCase() + header.key.slice(1)"
+            ></v-text-field>
+          </div>
         </template>
         <!-- <template v-slot:text>
           <v-text-field
@@ -40,10 +42,11 @@
         </template> -->
         <template v-slot:top>
           <v-toolbar flat>
-            <v-toolbar-title>My CRUD</v-toolbar-title>
+            <v-toolbar-title>Cluster Prospect</v-toolbar-title>
             <v-divider class="mx-4" inset vertical></v-divider>
             <v-spacer></v-spacer>
-            <v-dialog v-model="dialog" max-width="500px">
+            <!-- <v-dialog v-model="dialog" max-width="500px"> -->
+            <v-dialog v-model="dialog" max-width="1000px">
               <v-card>
                 <v-card-title>
                   <span class="text-h5">Edit</span>
@@ -51,7 +54,10 @@
                 <v-card-text>
                   <v-container>
                     <v-row>
-                      <v-col cols="12" md="4" sm="6">
+                      <v-col  cols="12" md="3" sm="6" v-for="(header) in headers.slice(0, headers.length-1)">
+                        <v-text-field v-model="editedItem[header.key]" :label="header.title" />
+                      </v-col>
+                      <!-- <v-col cols="12" md="4" sm="6">
                         <v-text-field v-model="editedItem.name" label="Dessert name"></v-text-field>
                       </v-col>
                       <v-col cols="12" md="4" sm="6">
@@ -65,7 +71,7 @@
                       </v-col>
                       <v-col cols="12" md="4" sm="6">
                         <v-text-field v-model.number="editedItem.protein" label="Protein (g)"></v-text-field>
-                      </v-col>
+                      </v-col> -->
                     </v-row>
                   </v-container>
                 </v-card-text>
@@ -92,7 +98,7 @@
                 </v-card-actions>
               </v-card>
             </v-dialog>
-            <v-dialog v-model="dialogUpload" max-width="500px">
+            <v-dialog v-model="dialogUpload" max-width="800px">
               <template v-slot:activator="{ props }">
                 <v-btn class="mb-2" color="primary" dark v-bind="props">
                   Bulk Upload (Create/Update)
@@ -145,6 +151,8 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   data: () => ({
     rules: [
@@ -165,33 +173,52 @@ export default {
     uploadedFile: [],
     headers: [
       {
-        title: 'Dessert (100g serving)',
+        title: 'Company Name',
         align: 'start',
-        sortable: false,
-        key: 'name',
+        sortable: true,
+        key: 'company_name',
+        minWidth: '200'
       },
-      { title: 'Calories', key: 'calories' },
-      { title: 'Fat (g)', key: 'fat' },
-      { title: 'Carbs (g)', key: 'carbs' },
-      { title: 'Protein (g)', key: 'protein' },
-      { title: 'Actions', key: 'actions', sortable: false },
+      { title: 'City', key: 'city',
+        minWidth: '200' },
+      { title: 'State', key: 'state',
+        minWidth: '200' },
+      { title: 'GSTIN', key: 'gstin',
+        minWidth: '200' },
+      { title: 'Sector', key: 'sector',
+        minWidth: '200' },
+      { title: 'Turnover', key: 'turnover',
+        minWidth: '200' },
+      { title: 'Industry', key: 'industry',
+        minWidth: '200' },
+      { title: 'POC', key: 'poc_name',
+        minWidth: '200' },
+      { title: 'POC Contact', key: 'poc_contact_no',
+        minWidth: '200' },
+      { title: 'POC Email', key: 'poc_email',
+        minWidth: '200' },
+      { title: 'POC Designation', key: 'poc_designation',
+        minWidth: '200' },
+      { title: 'IndiaMart', key: 'indiamart_link',
+        minWidth: '200' },
+      { title: 'Website', key: 'website',
+        minWidth: '200' },
+      { title: 'Product', key: 'mfg_product',
+        minWidth: '200' },
+      { title: 'Quality Tag', key: 'quality_tag',
+        minWidth: '200' },
+      { title: 'Deal ID', key: 'hb_deal_id',
+        minWidth: '200' },
+      { title: 'Deal Creation Date', key: 'hb_deal_creation_date',
+        minWidth: '200' },
+
+      { title: 'Actions', key: 'actions', sortable: false,
+      fixed: true, minWidth: '200', pinned: 'right',  },
     ],
     desserts: [],
     editedIndex: -1,
-    editedItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
-    defaultItem: {
-      name: '',
-      calories: 0,
-      fat: 0,
-      carbs: 0,
-      protein: 0,
-    },
+    editedItem: {},
+    defaultItem: {},
   }),
 
   computed: {
@@ -258,83 +285,26 @@ export default {
   },
 
   created() {
+    this.initialiseEditedItem()
     this.initialize()
   },
 
   methods: {
-    initialize() {
-      this.desserts = [
-        {
-          name: 'Frozen Yogurt',
-          calories: 159,
-          fat: 6.0,
-          carbs: 24,
-          protein: 4.0,
-        },
-        {
-          name: 'Ice cream sandwich',
-          calories: 237,
-          fat: 9.0,
-          carbs: 37,
-          protein: 4.3,
-        },
-        {
-          name: 'Eclair',
-          calories: 262,
-          fat: 16.0,
-          carbs: 23,
-          protein: 6.0,
-        },
-        {
-          name: 'Cupcake',
-          calories: 305,
-          fat: 3.7,
-          carbs: 67,
-          protein: 4.3,
-        },
-        {
-          name: 'Gingerbread',
-          calories: 356,
-          fat: 16.0,
-          carbs: 49,
-          protein: 3.9,
-        },
-        {
-          name: 'Jelly bean',
-          calories: 375,
-          fat: 0.0,
-          carbs: 94,
-          protein: 0.0,
-        },
-        {
-          name: 'Lollipop',
-          calories: 392,
-          fat: 0.2,
-          carbs: 98,
-          protein: 0,
-        },
-        {
-          name: 'Honeycomb',
-          calories: 408,
-          fat: 3.2,
-          carbs: 87,
-          protein: 6.5,
-        },
-        {
-          name: 'Donut',
-          calories: 452,
-          fat: 25.0,
-          carbs: 51,
-          protein: 4.9,
-        },
-        {
-          name: 'KitKat',
-          calories: 518,
-          fat: 26.0,
-          carbs: 65,
-          protein: 7,
-        },
-      ]
+    initialiseEditedItem() {
+      this.editedItem = {}
+
+      for(var i=0; i<this.headers.length; ++i) {
+        const key = this.headers[i].key
+        this.editedItem.key = ''
+      }
+
+      this.defaultItem = this.editedItem
+
+    },
+    async initialize() {
+      const fetchedTokenResponse = await axios.get("http://localhost:56777/tz-cpd/get-prospect");
+      console.log('DUBEY: ', JSON.stringify(fetchedTokenResponse))
+      this.desserts = fetchedTokenResponse.data.data;
     },
 
     editItem(item) {
